fc2b86a365723be90af5ddf35b887c3f4dcf577c
KennethQiu <kennethqiujunhao@gmail.com>
2016-08-16 03:23:53 +0000

    completed tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'posts',     to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
-  get 'posts/:id', to: 'posts#show'
-  post 'posts',    to: 'posts#create'
+  resources :posts
+  root to: 'posts#index'
 
 end
